318e48cea3e827192e8e0dd90304c91dd2581ee6
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-30 14:00:35 +0530

    check palindrome for date formats

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ function isPalindrome(str){
 }
 
 
-function convertdateTostring(date){
+function convertDateToString(date){
 
-  let dateStr = {day: '',month: '',year: ''};
- 
+ let dateStr = {day: '',month: '',year: ''};
+
   if(date.day < 10){
     dateStr.day = '0' + date.day;
   }else{
     dateStr.day = date.day.toString();
   }
-  
+
   if(date.month < 10){
     dateStr.month = '0' + date.month;
   }else{
@@ -38,50 +38,41 @@ function convertdateTostring(date){
 
   dateStr.year = date.year.toString();
 
-  return dateStr;
+  return  dateStr;
 
 }
 
-function getAllDateFormats(date){
- let dateStr =  convertdateTostring(date);
 
- let ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
- let mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
- let yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
- let ddmmyy =   dateStr.day + dateStr.month + dateStr.year.slice(-2);
- let mmddyy =   dateStr.month + dateStr.day + dateStr.year.slice(-2);
- let yymmdd  =  dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+function getAllDateFormats(date){
+let dateStr = convertDateToString(date);
+
+let ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+let mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+let yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+let ddmmyy =  dateStr.day + dateStr.month + dateStr.year.slice(-2);
+let mmddyy =  dateStr.month + dateStr.day + dateStr.year.slice(-2);
+let yymmdd =  dateStr.year.slice(-2) + dateStr.month + dateStr.day;
 
 return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd];
 
-}
+};
 
-
+ 
 function checkPalindromeForAllDateFormats(date){
+ 
+  let listOfPalindromes = getAllDateFormats(date);
+  let flag = false;
 
-  let listofDateformats = getAllDateFormats(date);
-  let flag = false
-
-  for(let i = 0; i < listofDateformats.length; i++){
-    if(isPalindrome(listofDateformats[i])){
-     flag = true;
-     break;
+  for(let i = 0; i < listOfPalindromes.length; i++){
+      if(isPalindrome(listOfPalindromes[i])){
+        flag = true;
+        break;
+      }
     }
-  }
+    
+    return flag;
+  };
 
-  return flag;
-}
+  let date = {day: '2',month: '11',year: '2020'};
 
-const date = {
-  day: 2,
-  month: 11,
-  year: 2020,
-}
-
-console.log(convertdateTostring(date));
-
-console.log(getAllDateFormats(date));
-
-console.log(checkPalindromeForAllDateFormats(date));
-
-
+  console.log(checkPalindromeForAllDateFormats(date))
